e474066d71946c6f079a0a2c27fa43efe2d46052
osahene <bra.kwameadu3@gmail.com>
2024-11-30 16:46:56 +0000

    card h-fit

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Cards = ({ cardName, cardName2, cardLogo, logoAlt }) => {
   return (
-    <div className="w-auto h-auto grid gap-auto backdrop-filter backdrop-blur-sm bg-opacity-10 bg-slate-200 overflow-hidden border border-gray-200 rounded-xl shadow shadow-xl">
+    <div className="w-auto h-fit grid gap-auto backdrop-filter backdrop-blur-sm bg-opacity-10 bg-slate-200 overflow-hidden border border-gray-200 rounded-xl shadow shadow-xl">
       <div className="p-1 flex flex-col justify-center items-start content-fit">
         <div className="ml-3 flex flex-col items-start">
           <p className="font-bold text-[24px] xs:text-[40px] sm:text-[50px]  text-gray-700 dark:text-white">
