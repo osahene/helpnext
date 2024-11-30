7132b0d190d159ea467cc7183afc54d9261b1884
osahene <bra.kwameadu3@gmail.com>
2024-11-30 17:38:54 +0000

    fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col h-[80vh]">
         <section className="flex-grow">
           <MainPage />
         </section>
