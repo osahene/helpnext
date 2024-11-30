ecc04e567dd2a4bcc396ae5f0c996c0b333d2daa
osahene <bra.kwameadu3@gmail.com>
2024-11-30 19:41:46 +0000

    main page h-screen

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,15 +6,17 @@ import MainPage from "@/components/Cards/pageCards";
 const Main = () => {
   return (
     <>
-      <Navbar />
-      <div className="flex flex-col h-[80vh]">
-        <section className="flex-grow">
-          <MainPage />
-        </section>
+      <div className="min-h-screen">
+        <Navbar />
+        <div className="flex flex-col ">
+          <section className="flex-grow">
+            <MainPage />
+          </section>
+        </div>
+        <div className="sticky">
+          <Footer />
+        </div>
       </div>
-      {/* <div className="sticky">
-        <Footer />
-      </div> */}
     </>
   );
 };
