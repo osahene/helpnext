2e2fee05c59349c73dc889ccbb44934625aa7741
osahene <bra.kwameadu3@gmail.com>
2024-11-30 17:24:28 +0000

    section height

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col h-[75vh]">
         {/* Main Content */}
         <section className="flex-grow">
           <MainPage />
