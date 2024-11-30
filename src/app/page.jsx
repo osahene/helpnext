feac26b55e5382bc5f46fd63b9b4bdce2b84fe46
osahene <bra.kwameadu3@gmail.com>
2024-11-30 17:09:14 +0000

    card fills the spaces

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Navbar from "@/components/Navbars/IndexNavbar";
-import FooterAdmin from "@/components/Footers/FooterAdmin";
+import Footer from "@/components/Footers/FooterAdmin";
 import MainPage from "@/components/Cards/pageCards";
 
 const Main = () => {
   return (
     <>
       <Navbar />
-      <section>
-        <MainPage />
-      </section>
-      <FooterAdmin />
+      <div className="flex flex-col min-h-screen">
+        {/* Main Content */}
+        <section className="flex-grow">
+          <MainPage />
+        </section>
+        {/* Footer */}
+        <Footer />
+      </div>
     </>
   );
 };
