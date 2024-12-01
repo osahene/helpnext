a68c16b644c633a53a56eba7681e2c05d1000573
osahene <bra.kwameadu3@gmail.com>
2024-12-01 05:24:05 +0000

    added a package manager to package.json

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
