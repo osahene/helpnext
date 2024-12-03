1f586f606a361fb2894a563d3a38fc67b0f8a157
osahene <bra.kwameadu3@gmail.com>
2024-12-03 20:07:38 +0000

    last page

---
+++ src/app/auth/verifyEmail/page.jsx
@@ -0,0 +1,11 @@
+const { default: VerifyEmail } = require("@/components/Auth/verifyEmail");
+const { default: store } = require("@/redux/store");
+const { Provider } = require("react-redux");
+
+export default function VerifyEmailPage() {
+  return (
+    <Provider store={store}>
+      <VerifyEmail />
+    </Provider>
+  );
+}
