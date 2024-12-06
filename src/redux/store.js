e4084e1aa25ef0966aeeee581e44819291c1f77b
osahene <bra.kwameadu3@gmail.com>
2024-12-06 14:56:44 +0000

    added persist to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 import authReducer from "./authSlice";
 import globalReducer from "./globalSlice"; // Optional: for global state like loading
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     global: globalReducer,
+    persistReduce: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
 
 export default store;
