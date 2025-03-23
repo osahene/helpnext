0b96756584b25eab627cde50e4a864923ed16409
osahene <bra.kwameadu3@gmail.com>
2025-03-23 19:39:14 +0000

    update login component to use full width for background container

--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -65,7 +65,7 @@ export default function Login() {
               />
               Help OO Help
             </a>
-            <div className="w-auto bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <div>
                   <h1 className="text-white text-lg text-center">
