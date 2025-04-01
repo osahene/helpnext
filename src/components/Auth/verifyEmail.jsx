2771d2f272d2c5495ca8693421d844b9f4a31e29
osahene <bra.kwameadu3@gmail.com>
2025-04-01 02:54:23 +0000

    apply custom dark background styling to email and phone verification components

--- src/components/Auth/verifyEmail.jsx
+++ src/components/Auth/verifyEmail.jsx
@@ -62,7 +62,7 @@ export default function VerifyEmail() {
   };
 
   return (
-    <div className="App-header">
+    <div className="App-header bg-cust-dark">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 sm:p-8">
