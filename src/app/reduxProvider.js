13281f98b251a6e98184deb0caeb95d407c06c22
osahene <bra.kwameadu3@gmail.com>
2025-04-29 14:51:52 +0000

    fix: reorder components in ReduxProvider for correct nesting of GoogleOAuthProvider

--- src/app/reduxProvider.js
+++ src/app/reduxProvider.js
@@ -7,12 +7,14 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function ReduxProvider({ children }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <GoogleOAuthProvider
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+        >
           {children}
-        </PersistGate>
-      </Provider>
-    </GoogleOAuthProvider>
+        </GoogleOAuthProvider>
+      </PersistGate>
+    </Provider>
   );
 }
